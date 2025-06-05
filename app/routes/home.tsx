ba5323872edf97987f23aab35ed23cac0ceccedc
HarrySeop <harryseop@gmail.com>
2025-06-06 03:12:53 +0900

    🔥Chore: 프로젝트 세팅 변경을 위한 초기화

--- app/routes/home.tsx
+++
@@ -1,9 +0,0 @@
-import type { Route } from './+types/home';
-
-export function meta({}: Route.MetaArgs) {
-  return [{ title: 'Kabana' }, { name: 'description', content: 'Welcome to Kabana!' }];
-}
-
-export default function Home() {
-  return <>이게 맞나?</>;
-}
