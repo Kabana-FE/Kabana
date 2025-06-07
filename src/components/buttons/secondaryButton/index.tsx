aa0515ffb823c0f6855b4cab9c1ee73dc58d8c52
leetaesik <vlwkvkd@naver.com>
2025-06-08 02:05:36 +0900

    📝Docs: jsdoc 추가

--- src/components/buttons/secondaryButton/index.tsx
+++ src/components/buttons/secondaryButton/index.tsx
@@ -7,6 +7,21 @@ const buttonVariants = {
   outlined: 'border border-gray-300 bg-white text-capybara hover:border-capybara',
 };
 
+/**
+ * secondary 버튼 컴포넌트입니다.
+ * 'variant'를 통해 스타일을 변경할 수 있고, 'className'으로 버튼의 크기를 정해주어야 합니다.
+ *
+ * @param {'filled'|'outlined'} variant 기본값은 'filled' 입니다.
+ * @param {string} className 추가적인 스타일을 받습니다.
+ * @param {React.ReactNode} children 버튼의 내용을 받습니다.
+ * @param {'button' | 'submit' | 'reset'} type 버튼의 타입을 받습니다. 기본값은 'button' 입니다.
+ * @param {function} onButtonClick 버튼이 눌렸을 때 실행될 함수를 받습니다.
+ * @example
+ * <SecondaryButton className='h-32 w-100 px-37 py-7 text-sm' variant='outlined'>
+ *   버튼 내용이 들어갑니다.
+ * </SecondaryButton>
+ */
+
 const SecondaryButton = ({ variant = 'filled', className, children, type = 'button', onButtonClick }: ButtonProps) => {
   return (
     <button
