778bdc612dc8207ec814bb03a99959fee6aa7c0c
Jihyun <wlguskim21@naver.com>
2025-06-07 16:13:34 +0900

    📝Docs: JSDoc 추가

--- src/components/button/pagenation/index.tsx
+++ src/components/button/pagenation/index.tsx
@@ -4,6 +4,15 @@ import arrowForward from '@/assets/icons/arrow_forward.svg';
 
 import type { PagenationButtonProps } from './types';
 
+/**
+ * 페이지네이션 버튼 컴포넌트입니다.
+ * 방향(left 또는 right)에 따라 아이콘과 스타일이 다르게 적용됩니다.
+ *
+ * @param {'left' | 'right'} direction  - 버튼 방향
+ * @param {boolean} isDisabled - 버튼 비활성화 여부
+ * @param {() => void} onButtonClick - 버튼 클릭 시 호출되는 함수
+ */
+
 const PagenationButton = ({ direction, isDisabled, onButtonClick }: PagenationButtonProps) => {
   const directionMap = {
     left: {
@@ -21,7 +30,7 @@ const PagenationButton = ({ direction, isDisabled, onButtonClick }: PagenationBu
 
   return (
     <button
-      className={`${style} size-40 cursor-pointer border border-gray-300`}
+      className={`${style} size-40 cursor-pointer border border-gray-300 disabled:cursor-not-allowed`}
       disabled={isDisabled}
       onClick={onButtonClick}
     >
