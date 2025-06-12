f569fbd5ed1503efc6a84a7eb0aac543af16eafe
sonsujin <sonsj33@gmail.com>
2025-06-13 00:34:53 +0900

    🔨Fix: x 아이콘 추가

--- src/components/dialog/Close.tsx
+++ src/components/dialog/Close.tsx
@@ -1,7 +1,13 @@
+import CloseIcon from '@/assets/icons/CloseIcon';
+
 import type { DialogCloseProp } from './types';
 
 const Close = ({ toggleModal }: DialogCloseProp) => {
-  return <button onClick={() => toggleModal()}>x</button>;
+  return (
+    <button onClick={() => toggleModal()}>
+      <CloseIcon size={12.9} />
+    </button>
+  );
 };
 
 export default Close;
