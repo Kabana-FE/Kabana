da6425781e2196cc7fc7845facb6d14cd4215cfb
Sienna <sgoldenbird@gmail.com>
2025-06-14 01:40:26 +0900

    ✨ Feat: 라우터에 계층적 에러 바운더리 설정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { ApiErrorBoundary } from '@/components/errorBoundary/ApiErrorBoundary';
+import { GlobalErrorBoundary } from '@/components/errorBoundary/GlobalErrorBoundary';
 import { ROUTES } from '@/constants/paths/routes';
-import { Error } from '@/pages';
 
 const { APP, SIGNIN, SIGNUP, MYPAGE, DASHBOARD_LIST, DASHBOARD_DETAIL, DASHBOARD_EDIT, NOT_FOUND } = ROUTES;
 
@@ -13,7 +14,7 @@ const router = createBrowserRouter([
       const { rootLoader } = await import('@/loaders/rootLoader');
       return { Component: Root, loader: rootLoader };
     },
-    errorElement: <Error />,
+    errorElement: <GlobalErrorBoundary />,
     children: [
       {
         index: true,
@@ -46,42 +47,46 @@ const router = createBrowserRouter([
           return { Component: DashboardLayout, loader: authGuardLoader };
         },
         children: [
-          // --- Auth Guard Routes (인증이 필요한 페이지들) ---
           {
-            path: MYPAGE,
-            lazy: async () => {
-              const { default: Component } = await import('@/pages/user/MyPage');
-              const { loader } = await import('@/loaders/myPage/myPageLoader');
-              const { action } = await import('@/actions/myPage/myPageAction');
-              return { Component, loader, action };
-            },
-          },
-          {
-            path: DASHBOARD_LIST,
-            lazy: async () => {
-              const { default: Component } = await import('@/pages/dashboards/DashboardList');
-              const { loader } = await import('@/loaders/dashboard/listLoader');
-              const { action } = await import('@/actions/dashboard/listAction');
-              return { Component, loader, action };
-            },
-          },
-          {
-            path: DASHBOARD_DETAIL,
-            lazy: async () => {
-              const { default: Component } = await import('@/pages/dashboards/DashboardDetail');
-              const { loader } = await import('@/loaders/dashboard/detailLoader');
-              const { action } = await import('@/actions/dashboard/detailAction');
-              return { Component, loader, action };
-            },
-          },
-          {
-            path: DASHBOARD_EDIT,
-            lazy: async () => {
-              const { default: Component } = await import('@/pages/dashboards/DashboardEdit');
-              const { loader } = await import('@/loaders/dashboard/editLoader');
-              const { action } = await import('@/actions/dashboard/editAction');
-              return { Component, loader, action };
-            },
+            errorElement: <ApiErrorBoundary />,
+            children: [
+              {
+                path: MYPAGE,
+                lazy: async () => {
+                  const { default: Component } = await import('@/pages/user/Mypage');
+                  const { loader } = await import('@/loaders/myPage/myPageLoader');
+                  const { action } = await import('@/actions/myPage/myPageAction');
+                  return { Component, loader, action };
+                },
+              },
+              {
+                path: DASHBOARD_LIST,
+                lazy: async () => {
+                  const { default: Component } = await import('@/pages/dashboards/DashboardList');
+                  const { loader } = await import('@/loaders/dashboard/listLoader');
+                  const { action } = await import('@/actions/dashboard/listAction');
+                  return { Component, loader, action };
+                },
+              },
+              {
+                path: DASHBOARD_DETAIL,
+                lazy: async () => {
+                  const { default: Component } = await import('@/pages/dashboards/DashboardDetail');
+                  const { loader } = await import('@/loaders/dashboard/detailLoader');
+                  const { action } = await import('@/actions/dashboard/detailAction');
+                  return { Component, loader, action };
+                },
+              },
+              {
+                path: DASHBOARD_EDIT,
+                lazy: async () => {
+                  const { default: Component } = await import('@/pages/dashboards/DashboardEdit');
+                  const { loader } = await import('@/loaders/dashboard/editLoader');
+                  const { action } = await import('@/actions/dashboard/editAction');
+                  return { Component, loader, action };
+                },
+              },
+            ],
           },
         ],
       },
