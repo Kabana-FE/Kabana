668d095bb983450537670b0ed949172d7ce534ad
HarrySeop <harryseop@gmail.com>
2025-06-13 15:15:36 +0900

    ✨Feat: Input 컴포넌트 용 타입 선언

---
+++ src/components/input/types/index.ts
@@ -0,0 +1,27 @@
+export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  children: React.ReactNode;
+}
+
+export interface ErrorMessageProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  type: string;
+}
+
+export interface TextAreaProps extends Omit<React.TextareaHTMLAttributes<HTMLTextAreaElement>, 'autoComplete'> {
+  type: 'textarea';
+  name: string;
+}
+
+export interface FileUploadProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  type: 'file';
+}
+
+export type FieldProps = TextInputProps | TextAreaProps | FileUploadProps;
+
+export interface RootProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
