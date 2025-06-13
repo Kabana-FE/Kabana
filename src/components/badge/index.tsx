6b591a66aa45bf1572d268a13db772564efe2690
sonsujin <sonsj33@gmail.com>
2025-06-14 02:07:31 +0900

    ✨ Feat: 뱃지 스타일 추가

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -2,7 +2,7 @@ import DotIcon from '@/assets/icons/DotIcon';
 
 const Badge = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className='flex w-fit items-center gap-6 rounded-full bg-[#FDF4D3] px-8 py-4'>
+    <div className='flex w-fit items-center gap-6 rounded-full bg-[#FDF4D3] px-8 py-4 overflow-ellipsis whitespace-nowrap'>
       <DotIcon />
       <span className='text-xs text-[#764d4d]'>{children}</span>
     </div>
