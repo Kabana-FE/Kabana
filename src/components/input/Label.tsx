86f79cc1387ed38b07bd0694bdb3a6a58b8cb43d
HarrySeop <harryseop@gmail.com>
2025-06-15 03:21:17 +0900

    ♻️Refactor: Label과 Input의 연결 안되는 문제 해결

--- src/components/input/Label.tsx
+++ src/components/input/Label.tsx
@@ -2,8 +2,12 @@ import { twMerge } from 'tailwind-merge';
 
 import type { LabelProps } from './types';
 
-const Label = ({ children, className }: LabelProps) => {
-  return <label className={twMerge('text-lg text-gray-700', className)}>{children}</label>;
+const Label = ({ children, className, htmlFor }: LabelProps) => {
+  return (
+    <label className={twMerge('text-lg text-gray-700', className)} htmlFor={htmlFor}>
+      {children}
+    </label>
+  );
 };
 
 export default Label;
