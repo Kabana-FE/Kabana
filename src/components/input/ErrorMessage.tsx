a015d4e2dec962d1653ddf4e7854060c761aac9b
HarrySeop <harryseop@gmail.com>
2025-06-16 03:51:51 +0900

    ♻️Refactor: ErrorMessage가 없을때는 null로 적용되도록 변경

--- src/components/input/ErrorMessage.tsx
+++ src/components/input/ErrorMessage.tsx
@@ -3,6 +3,8 @@ import { twMerge } from 'tailwind-merge';
 import type { ErrorMessageProps } from './types';
 
 const ErrorMessage = ({ children, className }: ErrorMessageProps) => {
+  if (!children) return null;
+
   return <p className={twMerge('text-md text-red', className)}>{children}</p>;
 };
 
