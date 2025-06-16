3f6232e700890b529d0f6bc3d0523dd24cc52a69
Jihyun <wlguskim21@naver.com>
2025-06-16 16:13:42 +0900

    ✨Feat: Footer 컴포넌트 생성

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -1,5 +1,22 @@
+import GithubIcon from '@/assets/icons/GithubIcon';
+
+import Button from '../button';
+
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className='flex items-center justify-between bg-black p-40 text-xs text-gray-400 tablet:text-lg pc:px-140'>
+      <span>@Kabana - 2025</span>
+      <Button
+        aria-label='Kabana Github'
+        as='a'
+        href='https://github.com/Kabana-FE/Kabana.git'
+        size='none'
+        variant='none'
+      >
+        <GithubIcon className='bg-black tablet:size-22' size={18} />
+      </Button>
+    </footer>
+  );
 };
 
 export default Footer;
