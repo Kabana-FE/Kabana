7a712821f063df04fe9ecd476e0239d7cd792d7f
HarrySeop <harryseop@gmail.com>
2025-06-17 15:28:03 +0900

    ♻️Refactor: api 부분 팀이 작성한 request 방식으로 변경

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,14 +1,20 @@
-import axiosInstance from '@/apis/base/axiosInstance';
+import { requestPost } from '@/apis/base/request';
 import { AUTH_ENDPOINTS } from '@/constants/paths';
-import type { LoginRequest, LoginResponse, SignupRequest } from '@/stores/types/auth';
-import type { User as SignupResponse } from '@/stores/types/user';
+import type { LoginRequest, LoginResponse, SignupRequest } from '@/schemas/auth';
+import type { User as SignupResponse } from '@/schemas/user';
 
+/**
+ * @description 로그인 요청 API
+ * @returns accessToken + user
+ */
 export const login = async (data: LoginRequest): Promise<LoginResponse> => {
-  const response = await axiosInstance.post<LoginResponse>(AUTH_ENDPOINTS.SIGNIN, data);
-  return response.data;
+  return await requestPost<LoginResponse, LoginRequest>(AUTH_ENDPOINTS.SIGNIN, data);
 };
 
+/**
+ * @description 회원가입 요청 API
+ * @returns 가입된 사용자 정보
+ */
 export const signup = async (data: SignupRequest): Promise<SignupResponse> => {
-  const response = await axiosInstance.post<SignupResponse>(AUTH_ENDPOINTS.SIGNUP, data);
-  return response.data;
+  return await requestPost<SignupResponse, SignupRequest>(AUTH_ENDPOINTS.SIGNUP, data);
 };
