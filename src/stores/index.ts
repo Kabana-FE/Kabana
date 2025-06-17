c29c59a5cd199862751f3a4131cd9adfc6a045c3
HarrySeop <harryseop@gmail.com>
2025-06-17 15:25:53 +0900

    ♻️Refactor: 사용안하는 Slice 제거 후, useKabana 하나로 변경

---
+++ src/stores/index.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+import createAuthSlice from './slices';
+import type { AuthSlice } from './types';
+
+export const useKabanaStore = create<AuthSlice>()(
+  persist(
+    (...args) => ({
+      ...createAuthSlice(...args),
+    }),
+    {
+      name: 'auth-storage',
+    },
+  ),
+);
