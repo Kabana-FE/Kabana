50ff8bd7239c56630f7a26128e95debc57f97ebf
sonsujin <sonsj33@gmail.com>
2025-06-19 22:23:55 +0900

    ✨Feat: 모달 및 상태 추가

--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router';
 
 import AddIcon from '@/assets/icons/AddIcon';
 import DotIcon from '@/assets/icons/DotIcon';
@@ -7,10 +8,17 @@ import CardItem from '@/components/cardList/CardItem';
 import Button from '@/components/common/button';
 
 import CreateTodo from '../modal/createTodo';
+import DeleteAlert from '../modal/DeleteAlert';
+import EditColumn from '../modal/EditColumn';
 import type { CardListType } from './types';
 
-const CardList = ({ data, title }: CardListType) => {
+const CardList = ({ data, title, columnId }: CardListType) => {
+  const location = useLocation();
+  const dashboardId = Number(location.pathname.split('/')[2]);
   const [createTodo, setCreateTodo] = useState(false);
+  const [editColumn, setEditColumn] = useState(false);
+  const [deleteAlert, setDeleteAlert] = useState(false);
+
   return (
     <div className='px-12 pc:flex-1/5'>
       <div className='mb-25 flex justify-between'>
@@ -21,18 +29,31 @@ const CardList = ({ data, title }: CardListType) => {
             {data.cards?.length}
           </span>
         </div>
-        <Button className='p-0' variant='none'>
+        <Button className='p-0' variant='none' onClick={() => setEditColumn(!editColumn)}>
           <SettingIcon />
         </Button>
       </div>
-      <Button className='mb-10 w-full' variant='outlined' onClick={() => setCreateTodo(!createTodo)}>
+      <Button className='mb-10 w-full' variant='outlined' onClick={() => setCreateTodo(true)}>
         <AddIcon />
       </Button>
       {data.cards &&
         data.cards.map((card, idx) => {
           return <CardItem key={`${card.id}-${idx}`} data={card} />;
         })}
-      <CreateTodo modalIsOpen={createTodo} toggleModal={() => setCreateTodo(!createTodo)} />
+      <CreateTodo
+        columnId={columnId}
+        dashboardId={dashboardId}
+        isModalOpen={createTodo}
+        toggleModal={() => setCreateTodo(!createTodo)}
+      />
+      <EditColumn
+        columnId={columnId}
+        initialTitle={title}
+        isModalOpen={editColumn}
+        toggleDeleteAlert={() => setDeleteAlert(!deleteAlert)}
+        toggleModal={() => setEditColumn(!editColumn)}
+      />
+      <DeleteAlert columnId={columnId} isModalOpen={deleteAlert} toggleModal={() => setDeleteAlert(!deleteAlert)} />
     </div>
   );
 };
--- src/components/cardList/types/index.ts
+++ src/components/cardList/types/index.ts
@@ -3,6 +3,7 @@ import type { CardType, GetCardListType } from '@/schemas/card';
 export interface CardListType {
   data: GetCardListType;
   title: string;
+  columnId: number;
 }
 
 export interface CardItemType {
