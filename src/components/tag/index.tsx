439c3edcd6144f959e56d34b14719ada0e0f00a0
sonsujin <sonsj33@gmail.com>
2025-06-20 01:32:30 +0900

    ✨Feat: 태그 컴포넌트 임의로 컬러 추가

--- src/components/tag/index.tsx
+++ src/components/tag/index.tsx
@@ -1,10 +1,21 @@
 import { twMerge } from 'tailwind-merge';
 
+import colorList from '@/constants/ui/colorList';
+
 import type TagType from './types';
 
 const Tag = ({ children, className, ...props }: TagType) => {
+  const getColor = (nickname: string) => {
+    const trimmed = nickname.trim();
+    if (!trimmed) return colorList[0];
+    const firstChar = trimmed.charAt(0).toUpperCase();
+    const index = firstChar.charCodeAt(0) % 10;
+    return colorList[index];
+  };
+
+  const tagColor = getColor(children as string);
   return (
-    <span className={twMerge('inline-flex w-auto px-10 py-2 text-[12px]', className)} {...props}>
+    <span className={twMerge(`inline-flex w-auto px-10 py-2 text-[12px] ${tagColor}`, className)} {...props}>
       {children}
     </span>
   );
